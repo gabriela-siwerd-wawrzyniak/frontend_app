59e22ae9732e949e66232c1fb5e0c30e2537fca4
Gabriela Siwerd-Wawrzyniak <g.siwerd@gmail.com>
2025-05-12 08:20:56 +0200

    feat: add prettier and linting scripts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,10 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import pluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 import pluginReact from 'eslint-plugin-react'
 
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], plugins: { js }, extends: ['js/recommended'] },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], plugins: { js } },
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], languageOptions: { globals: globals.browser } },
   {
     ...pluginReact.configs.flat.recommended,
@@ -13,4 +14,5 @@ export default [
       }
     }
   },
+  pluginPrettierRecommended
 ]

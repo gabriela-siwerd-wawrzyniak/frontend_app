273abfcaaebbcdcbd1108ba50b9abf22b6b81cce
Gabriela Siwerd-Wawrzyniak <g.siwerd@gmail.com>
2025-05-12 21:22:38 +0200

    feat: improve ESLint config to support TS

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,17 +2,39 @@ import js from '@eslint/js'
 import globals from 'globals'
 import pluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 import pluginReact from 'eslint-plugin-react'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+
 
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], plugins: { js } },
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], languageOptions: { globals: globals.browser } },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 'latest',
+        project: './tsconfig.json',
+        tsconfigRootDir: process.cwd(),
+      },
+      globals: globals.browser
+    },
+    plugins: {
+      js,
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn'],
+    },
+  },
   {
     ...pluginReact.configs.flat.recommended,
     settings: {
       react: {
-        version: 'detect'
-      }
-    }
+        version: 'detect',
+      },
+    },
   },
-  pluginPrettierRecommended
-]
+  pluginPrettierRecommended,
+];

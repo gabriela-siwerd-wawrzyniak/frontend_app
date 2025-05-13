bbe23ac141233a73930807a000984087640367ef
Gabriela Siwerd-Wawrzyniak <g.siwerd@gmail.com>
2025-05-13 11:16:18 +0200

    feat: update README, use env variables for API access

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,2 +1,2 @@
-export const API_URL = 'https://my.api.mockaroo.com/login';
-export const API_KEY = 'ed97eff0';
+export const API_URL = process.env.REACT_APP_API_URL!;
+export const API_KEY = process.env.REACT_APP_API_KEY!;
